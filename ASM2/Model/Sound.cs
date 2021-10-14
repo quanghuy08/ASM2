90b61d47523c6e1178976ffde5bbcc338249771e
quanghuy <huynqth2008064@fpt.edu.vn>
2021-10-14 21:56:13 +0700

    Add project files.

---
+++ ASM2/Model/Sound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM2.Model
+{
+    public class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+
+        public Sound(string name, SoundCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = String.Format("/Assets/Audio/{0}/{1}.mp3", category, name);
+            ImageFile = String.Format("/Assets/Images/{0}/{1}.jpg", category, name);
+        }
+    }
+
+    public enum SoundCategory
+    {
+        Vibrant,
+        Sad,
+    }
+}
---
+++ ASM2/Model/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM2.Model
+{
+    public class SoundManager
+    {
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear();
+            allSounds.ForEach(p => sounds.Add(p));
+        }
+
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundCategory)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Category == soundCategory).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+
+        public static void GetSoundsByName(ObservableCollection<Sound> sounds, string name)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Name == name).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("AiNo1", SoundCategory.Vibrant));
+            sounds.Add(new Sound("CuoiThoi", SoundCategory.Vibrant));
+
+            sounds.Add(new Sound("CanDuyen", SoundCategory.Sad));
+            sounds.Add(new Sound("ChiAnhConYeu", SoundCategory.Sad));
+
+            return sounds;
+        }
+    }
+}
